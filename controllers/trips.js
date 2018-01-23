ff7e2756a160e9869c8dbb0145e0e83ab3ae867a
NickDErrico <nickderrico@hotmail.com>
2018-01-23 14:36:39 -0700

    responsive flight selector

--- controllers/trips.js
+++ controllers/trips.js
@@ -3,16 +3,25 @@ const knex = require("../db/knex.js");
 module.exports = {
 
   index: (req, res) => {
-    if (req.session.user) {
-      knex('users')
-        .where('id', req.session.user)
-        .then((dbRes) => {
-          res.render('trips', { user: dbRes[0] })
-        })
-    } else {
-      res.render('trips', { user: {} })
-    }
+    knex('flights')
+      .then((dbFlights) => {
+        if (req.session.user) {
+          knex('users')
+            .where('id', req.session.user)
+            .then((dbRes) => {
+              res.render('trips', { user: dbRes[0], flights: dbFlights })
+            })
+        }
+        res.render('trips', { user: {}, flights: dbFlights })
+      })
   },
+  planTrip: (req, res) => {
+    knex('flights')
+      .joins('airlines', 'airlines.id', 'airline_id')
+      .then(() => {
+        res.redirect('/trips')
+      })
+  }
 
 
 }
