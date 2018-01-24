75bb4ffbe4f47b4b86d59c631b621b413d86a942
NickDErrico <nickderrico@hotmail.com>
2018-01-23 18:25:13 -0700

    working on trips post route

--- config/routes.js
+++ config/routes.js
@@ -1,14 +1,17 @@
 const users = require("../controllers/users.js")
 const trips = require("../controllers/trips.js")
+const airlines = require("../controllers/airlines.js")
 module.exports = function(app) {
 
 
   // HOME :
   app.get('/', users.index);
 
-  app.post('/users', users.create)
+  app.post('/users', users.create);
 
   // TRIPS :
-  app.get('/trips/', trips.index);
+  app.get('/trips', trips.index);
+
+  app.post('/trips', trips.create);
 
 }
--- controllers/trips.js
+++ controllers/trips.js
@@ -3,23 +3,36 @@ const knex = require("../db/knex.js");
 module.exports = {
 
   index: (req, res) => {
-    knex('flights')
-      .then((dbFlights) => {
-        if (req.session.user) {
-          knex('users')
-            .where('id', req.session.user)
-            .then((dbRes) => {
-              res.render('trips', { user: dbRes[0], flights: dbFlights })
-            })
-        }
-        res.render('trips', { user: {}, flights: dbFlights })
+    knex('trips')
+      .where('user_id', req.session.user)
+      .then((dbTrips) => {
+        knex('flights')
+          .then((dbFlights) => {
+            if (req.session.user) {
+              knex('users')
+                .where('id', req.session.user)
+                .then((dbRes) => {
+                  res.render('trips', { user: dbRes[0], flights: dbFlights, trip: dbTrips })
+                })
+            } else {
+              res.render('trips', { user: {}, flights: dbFlights, trip: dbTrips })
+            }
+          })
       })
   },
-  planTrip: (req, res) => {
-    knex('flights')
-      .joins('airlines', 'airlines.id', 'airline_id')
-      .then(() => {
-        res.redirect('/trips')
+  create: (req, res) => {
+    knex('trips')
+      .insert({
+        user_id: req.session.user,
+        flight_id: req.body.flight_id,
+        trip_description: req.body.trip_description,
+        title: req.body.title
+      }, '*')
+      .then((trip) => {
+        req.session.trip = trip[0].id
+        req.session.save(() => {
+          res.redirect('/trips')
+        })
       })
   }
 
